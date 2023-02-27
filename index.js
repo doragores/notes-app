8e587c882ba4711fe883050ec5b14a24604cc82e
Dora Gores <d.lopesgores@gmail.com>
2023-02-27 15:48:08 +0000

    added code to return array in console log

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 console.log('The notes app is running');
+const NotesModel = require('./notesModel')
 
+
+console.log('The notes app is running');
+
+
+let notemodel = new NotesModel;
+notemodel.addNote('Buy Milk');
+notemodel.addNote('Go to the Gym');
+
+console.log(notemodel.getNotes());
